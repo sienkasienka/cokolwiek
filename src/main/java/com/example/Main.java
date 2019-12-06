dc4f39fd9343d251e74c9f5fd5a0306f121b9d57
MSI <sienka_sienka@op.pl>
2019-12-06 18:51:38 +0100

    Revert "Unneeded line"
    This reverts commit b7dfd35e

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("cokolwiek");
         System.out.println("ten tekst jest wazniejszy");
+        System.out.println("rozwiazalem konflikty");
     }
 
 }
