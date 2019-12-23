64e31714100bac82ce4e9804edfbf1a38c45e8d3
sienkasienka <1Gnusmas!>
2019-12-23 14:55:49 +0100

    add one line

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("cokolwiek");
         System.out.println("ten tekst jest wazniejszy");
+        System.out.println("dodano ta linijke od testow");
         System.out.println("2 Testy na branchu temporary  ");
     }
 
