d314b08c6ef1e1a68f373ab9b948e10f577ecf0e
sienkasienka <32477659+sienkasienka@users.noreply.github.com>
2019-12-23 14:46:29 +0100

    two lines removed

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,8 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("cokolwiek");
         System.out.println("ten tekst jest wazniejszy");
-        System.out.println("TEST !@#$");
-        System.out.println("Testy na branchu temporary  ");
         System.out.println("2 Testy na branchu temporary  ");
     }
 
